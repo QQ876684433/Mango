05eff89c34ee81fcf7ef0bf8418eb2a31a4a6d60
lfm <1071956678@qq.com>
2019-06-07 11:32:43 +0800

    200状态码默认添加cache

--- HttpMessage/src/main/java/service/statusReceiver/OkHandler.java
+++ HttpMessage/src/main/java/service/statusReceiver/OkHandler.java
@@ -2,11 +2,14 @@ package service.statusReceiver;
 
 import http.core.HttpResponse;
 import http.util.HttpStatus;
+import http.util.header.ResponseHeader;
+import service.FileUtils;
 import service.fileTrans.*;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,10 +24,11 @@ public class OkHandler implements BaseHandler {
      * <p>
      * POST:一个描述或包含动作结果的实体;
      *
+     * 默认进行cache缓存
      * @return
      */
     @Override
-    public Map<String, Object> handleStatus(HttpResponse response) {
+    public Map<String, Object> handleStatus(HttpResponse response) throws IOException {
         //TODO: GET,POST 请求分析数据类型(content-Type),缓存本地,给出路径展示;
         switch (response.getHeader().getProperty("Content-Type")) {
             case "text/plain":
@@ -43,6 +47,11 @@ public class OkHandler implements BaseHandler {
         Map<String, Object> map = new HashMap<>();
         map.put("status", HttpStatus.CODE_200);
         map.put("file", this.fileTransformer.transform(response.getResponseBodyStream()));
+        if (null == response.getHeader().getProperty(ResponseHeader.CACHE_CONTROLL) ||
+                !response.getHeader()
+                        .getProperty(ResponseHeader.CACHE_CONTROLL).equals("no-cache")) { //已指定不需要设置缓存
+            map.put("cache-location", FileUtils.cacheResponse(response, "/cache/"));
+        }
         return map;
     }
 }
